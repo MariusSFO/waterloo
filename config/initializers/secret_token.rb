3d0fe69328163c1da3d661862cd76f8620bec6d7
Marius <marius.sewing@gmail.com>
2013-07-06 16:07:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waterloo::Application.config.secret_key_base = 'e991d479af6964e96750085c0f793b304c08b2a2d4dd3e88abf03094f32a9a4235ee384e53b63316e57d425a03be2c4106b38efb86be5081589ef0f446b2d693'
